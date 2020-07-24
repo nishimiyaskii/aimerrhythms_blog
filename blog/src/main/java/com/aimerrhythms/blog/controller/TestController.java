1a0a5d01feb7d22daec63f6c1289a240c0008010
nishimiyaskii <61838614+nishimiyaskii@users.noreply.github.com>
2020-07-24 18:27:31 +0800

    1

---
+++ blog/src/main/java/com/aimerrhythms/blog/controller/TestController.java
@@ -0,0 +1,41 @@
+package com.aimerrhythms.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author aimerrhythms
+ * @data 2020/7/21 17:36
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @RequestMapping("/types")
+    public String types() {
+        return "types";
+
+    }
+    @RequestMapping("/tags")
+    public String tags() {
+        return "tags";
+    }
+
+    @RequestMapping("/archives")
+    public String archives() {
+        return "archives";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+}
